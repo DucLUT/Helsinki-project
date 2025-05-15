2f1f9d0da55311c711fcbcf39cdaef7df33587d4
DucLUT <minhduc16032004@gmail.com>
2025-05-16 02:20:12 +0300

    cleaning logs

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,90 +1,89 @@
 import { createSlice } from "@reduxjs/toolkit";
-import toast from "react-hot-toast"
+import toast from "react-hot-toast";
 import { check, signup, logout, login } from "../services/auth";
 import { initializeSocket, disconnectSocket } from "../socket/socket";
 
 const authSlice = createSlice({
-    name:"auth",
-    initialState: {
-        authUser: null,
-        loading: true,
+  name: "auth",
+  initialState: {
+    authUser: null,
+    loading: true,
+  },
+  reducers: {
+    setAuthUser: (state, action) => {
+      state.authUser = action.payload;
     },
-    reducers: {
-        setAuthUser: (state, action) => {
-            state.authUser = action.payload;
-        },
-        clearAuthUser: (state) => {
-            state.authUser = null;
-        },
-        setLoading: (state, action) => {
-            state.loading = action.payload;
-        },
+    clearAuthUser: (state) => {
+      state.authUser = null;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+  },
+});
 
-    }
-})
-
-export const { setAuthUser, clearAuthUser , setLoading} = authSlice.actions;
+export const { setAuthUser, clearAuthUser, setLoading } = authSlice.actions;
 
 export const checkAuth = () => {
-    return async (dispatch) => {
-        dispatch(setLoading(true)); // Start loading
-        try {
-            const res = await check(); 
-            dispatch(setAuthUser(res)); 
-            initializeSocket(res.user._id);
-        } catch (error) {
-            console.error("Error during checkAuth:", error);
-            dispatch(clearAuthUser()); 
-        } finally {
-            dispatch(setLoading(false)); // End loading
-        }
-    };
+  return async (dispatch) => {
+    dispatch(setLoading(true)); // Start loading
+    try {
+      const res = await check();
+      dispatch(setAuthUser(res));
+      initializeSocket(res.user._id);
+    } catch (error) {
+      console.error("Error during checkAuth:", error);
+      dispatch(clearAuthUser());
+    } finally {
+      dispatch(setLoading(false)); // End loading
+    }
+  };
 };
 
-
 export const signupUser = (data) => {
-    return async (dispatch) => {
-        try {
-            const res = await signup(data); 
-            toast.success("Sign up successfully");
-            dispatch(setAuthUser(res)); 
-            initializeSocket(res.user._id);
-        } catch (error) {
-            console.error("Error during signup:", error);
-            const errorMessage = error.response?.data?.message || "An error occurred during signup";
-            toast.error(errorMessage);
-        }
-    };
+  return async (dispatch) => {
+    try {
+      const res = await signup(data);
+      toast.success("Sign up successfully");
+      dispatch(setAuthUser(res));
+      initializeSocket(res.user._id);
+    } catch (error) {
+      console.error("Error during signup:", error);
+      const errorMessage =
+        error.response?.data?.message || "An error occurred during signup";
+      toast.error(errorMessage);
+    }
+  };
 };
 
 export const loginUser = (data) => {
-    return async (dispatch) => {
-        try {
-            const res = await login(data);
-            console.log("Login response:", res);
-            toast.success("Logged in successfully");
-            dispatch(setAuthUser(res)); 
-            initializeSocket(res.user._id);
-        } catch (error) {
-            console.error("Error during login:", error);
-            const errorMessage = error.response?.data?.message || "An error occurred during login";
-            toast.error(errorMessage);
-        }
-    };
+  return async (dispatch) => {
+    try {
+      const res = await login(data);
+      toast.success("Logged in successfully");
+      dispatch(setAuthUser(res));
+      initializeSocket(res.user._id);
+    } catch (error) {
+      console.error("Error during login:", error);
+      const errorMessage =
+        error.response?.data?.message || "An error occurred during login";
+      toast.error(errorMessage);
+    }
+  };
 };
 
 export const logOut = () => {
-    return async (dispatch) => {
-        try {
-            await logout(); 
-            dispatch(clearAuthUser());
-            disconnectSocket();
-            toast.success("Logged out successfully");
-        } catch (error) {
-            console.error("Error during logout:", error);
-            toast.error("An error occurred during logout");
-        }
-    };
+  return async (dispatch) => {
+    try {
+      await logout();
+      dispatch(clearAuthUser());
+      disconnectSocket();
+      toast.success("Logged out successfully");
+    } catch (error) {
+      console.error("Error during logout:", error);
+      toast.error("An error occurred during logout");
+    }
+  };
 };
 
-export default authSlice.reducer
+export default authSlice.reducer;
--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,36 +1,34 @@
 import io from "socket.io-client";
 const isDevelopment = import.meta.env.MODE === "development";
 let SOCKET_URL;
-console.log("fuck shiet", import.meta.env.MODE)
-console.log("hihihiih",isDevelopment)
 if (isDevelopment) {
-	SOCKET_URL = "http://localhost:8080";
-}else{
-	SOCKET_URL = "/";
+  SOCKET_URL = "http://localhost:8080";
+} else {
+  SOCKET_URL = "/";
 }
-console.log("SOCKET_URL_CLIENT", SOCKET_URL)
+
 let socket = null;
 
 export const initializeSocket = (userId) => {
-	if (socket) {
-		socket.disconnect();
-	}
+  if (socket) {
+    socket.disconnect();
+  }
 
-	socket = io(SOCKET_URL, {
-		auth: { userId },
-	});
+  socket = io(SOCKET_URL, {
+    auth: { userId },
+  });
 };
 
 export const getSocket = () => {
-	if (!socket) {
-		throw new Error("Socket not initialized");
-	}
-	return socket;
+  if (!socket) {
+    throw new Error("Socket not initialized");
+  }
+  return socket;
 };
 
 export const disconnectSocket = () => {
-	if (socket) {
-		socket.disconnect();
-		socket = null;
-	}
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
 };
