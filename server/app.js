6c17bf659e8ab828d55b21a6daeb1026bf0d0848
DucLUT <minhduc16032004@gmail.com>
2025-05-15 20:51:31 +0300

    testing for large payload

--- server/app.js
+++ server/app.js
@@ -34,6 +34,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
